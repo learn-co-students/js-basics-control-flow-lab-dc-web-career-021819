eb847e2d20246163806c2660e3fe2b59b99dd18f
stephenyoon84 <stephenyoon84@gmail.com>
2019-03-30 00:53:14 -0400

    add solution

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your code in this file!
+function scuberGreetingForFeet(someNumber) {
+  if (someNumber <= 400) {
+    return "This one is on me!";
+  } else if (someNumber > 2000 && someNumber <= 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (someNumber > 2500) {
+    return "No can do.";
+  }
+}
+
+function ternaryCheckCity(someCity) {
+  return someCity === 'NYC' ? "Ok, sounds good." : "No go.";
+}
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case "generous":
+      return "Thank you so much.";
+      break;
+    case "not as generous":
+      return "Thank you.";
+      break;
+    default:
+      return "Bye."
+      break;
+  }
+}
